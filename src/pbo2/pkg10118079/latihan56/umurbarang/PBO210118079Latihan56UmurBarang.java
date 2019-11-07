9d4a6ca8a6ef8b7da285ddad32c13db552120456
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-11-07 22:22:25 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118079/latihan56/umurbarang/PBO210118079Latihan56UmurBarang.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118079.latihan56.umurbarang;
+
+/**
+ *
+ * @author User
+ */
+public class PBO210118079Latihan56UmurBarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 234;
+        // TODO code application logic here
+        barangantik hasil = new barangantik(umur);
+        radio hasil1 = new radio(umur);
+        hasil1.setName("Radio");
+        hasil1.getName();
+        hasil.umur =(234);
+        hasil.tampilumur();
+    }
+    
+}
---
+++ src/pbo2/pkg10118079/latihan56/umurbarang/barangantik.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118079.latihan56.umurbarang;
+
+/**
+ *
+ * @author User
+ */
+public class barangantik {
+   int umur;
+
+    public barangantik(int umur) {
+        this.umur = umur;
+    }
+    
+    void tampilumur(){
+       
+        System.out.println("Umur barang antik ini adalah : "+umur+" tahun");
+    }
+    
+}
